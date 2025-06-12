c0eebad2fa322c817d50dbbcc8625cb53dc7917d
cstokio <ti.cassiosousa@gmail.com>
2025-06-12 17:33:37 -0300

    Inicializando projeto - adicionando frontend e backend

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 // Fetch ou Axios
 import axios from "axios";
 
-const URL = "http://20.12.28.48:3001";
+const URL = "http://172.177.209.3:3001";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
